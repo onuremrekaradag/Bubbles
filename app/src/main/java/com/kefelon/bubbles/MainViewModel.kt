f3754772611b60bed49e90e8a8080a317ac52b93
Onur Emre Karadag <karadagonuremre@gmail.com>
2023-08-08 20:42:45 +0200

    Unnecessary imports deleted.

--- app/src/main/java/com/kefelon/bubbles/MainViewModel.kt
+++ app/src/main/java/com/kefelon/bubbles/MainViewModel.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
